47125d2bf87b6f628336c572dc26c0ca1548c594
Erick Silva <ericksilvaredes@gmail.com>
2023-09-05 11:41:14 -0300

    feat: update user

--- src/main/java/com/eiericksilva/todolist/services/UserService.java
+++ src/main/java/com/eiericksilva/todolist/services/UserService.java
@@ -25,4 +25,16 @@ public class UserService {
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
+
+    public User update(Long id, User newUserData) {
+        User userRef = userRepository.getReferenceById(id);
+        updateUserData(userRef, newUserData);
+        return userRepository.save(userRef);
+    }
+
+    private void updateUserData(User userRef, User newUserData) {
+        userRef.setName(newUserData.getName());
+        userRef.setPassword(newUserData.getPassword());
+    }
+
 }
