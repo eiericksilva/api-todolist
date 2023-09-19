a2855b8b9f09318e484396d5de454b90b4073bab
Erick Silva <ericksilvaredes@gmail.com>
2023-09-18 21:14:38 -0300

    clean: remove annotation lombok

--- src/main/java/com/eiericksilva/todolist/dto/response/UserResponseDto.java
+++ src/main/java/com/eiericksilva/todolist/dto/response/UserResponseDto.java
@@ -1,20 +1,44 @@
 package com.eiericksilva.todolist.dto.response;
 
 import com.eiericksilva.todolist.entities.Task;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.util.List;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class UserResponseDto {
     private Long id;
     private String name;
     private List<Task> tasks;
 
+    public UserResponseDto() {
+    }
+
+    public UserResponseDto(Long id, String name, List<Task> tasks) {
+        this.id = id;
+        this.name = name;
+        this.tasks = tasks;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
